780d1c74b479095d95e7aa781758eb135e6b37a0
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-07-13 11:31:31 -0300

    Tratamento de exceções
    exploramos como implementar o tratamento de exceções em uma API usando o Express. Começamos instalando a biblioteca express-async-errors e criamos uma classe AppError para gerenciar erros personalizados. Em seguida, configuramos um middleware para lidar com erros, incluindo validações com a biblioteca Zod. Demonstrei como lançar erros e retornar mensagens apropriadas para o cliente, garantindo que a API responda de forma clara e organizada em caso de falhas.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
+import { errorHandler } from './middlewares/error-handling';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(errorHandler);
 
 export{ app };
