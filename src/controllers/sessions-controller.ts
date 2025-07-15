8301f694e9bad08b96e71353b07e66547714eff1
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-07-15 20:39:07 -0300

    Recuperando o usuário
    recuperação de usuários utilizando o Prisma. Começamos importando o Prisma e buscando o usuário pelo e-mail. Se não encontrarmos, lançamos uma exceção para evitar que o usuário saiba se o erro está no e-mail ou na senha. Em seguida, comparamos a senha fornecida com a armazenada no banco usando bcrypt. Se houver um erro, novamente lançamos uma exceção genérica. Ao final, testamos as validações para garantir que tudo funcione corretamente.

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from "express";
+import { prisma } from "../database/prisma";
+import { AppError } from "../utils/AppError";
+import { compare } from "bcrypt";
+import { z } from "zod";
 
 class SessionsController {
-    async create(request: Request, response: Response) {
-        response.json({message: "ok"});
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z
+        .string()
+        .email({ message: "O email é invalido" }),
+      password: z
+        .string()
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if(!user) {
+      throw new AppError("Usuário ou senha inválidos", 401);
     }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if(!passwordMatch) {
+      throw new AppError("Usuário ou senha inválidos", 401);
+    }
+
+    response.json({ email, password });
+  }
 }
 
-export { SessionsController }
+export { SessionsController };
