6a84aba56fd945c3367e4bf7663bd2d210dc0a83
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-07-14 23:13:16 -0300

    Validando dados do usuário
    Validar os dados necessários para cadastrar um usuário utilizando a biblioteca Zod. Começamos criando um esquema que define as regras para o nome, e-mail e senha, incluindo mensagens de erro personalizadas. Também abordamos a configuração de roles, como "employee" e "manager". Demonstrei como testar a validação, mostrando como erros são retornados quando os dados não atendem aos critérios. Por fim, discutimos a importância de importar o Express corretamente para o funcionamento da validação.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,28 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { UserRole } from "@prisma/client";
+import { z } from "zod";
 
 class UsersController {
-    async create(request: Request, response: Response) {
-        response.json({message: "ok"})
-    }
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "O nome é obrigatorio" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "O email é invalido" })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+      role: z
+        .enum([UserRole.employee, UserRole.manager])
+        .default(UserRole.employee),
+    });
+
+    const { name, email, password, role } = bodySchema.parse(request.body);
+
+    response.json({ name, email, password, role });
+  }
 }
 
-export {UsersController}
+export { UsersController };
