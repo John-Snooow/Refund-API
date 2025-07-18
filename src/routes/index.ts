8b8b6e05e50a90594126f9a9801c7defbd891f9b
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-07-17 22:04:37 -0300

    Controller e rota de refunds
     vamos explorar como implementar rotas privadas em nossa API, garantindo que apenas usuários autenticados possam acessá-las. Começamos criando a estrutura para a rota de reembolsos e configurando o Insomnia para testar as requisições. Em seguida, montamos a estrutura do controller e das rotas, utilizando o Express para gerenciar as requisições. O foco é estabelecer uma base sólida para autenticação e autorização, preparando o terreno para os próximos passos no desenvolvimento da API.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
 import { usersRoutes } from "./users-routes";
+import { refundsRoutes } from "./refunds-routes";
 import { sessionsRoutes } from "./sessions-routes";
 
 const routes = Router()
 
+//rotas públicas
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
+//rotas privadas 
+routes.use("/refunds", refundsRoutes)
 export { routes }
